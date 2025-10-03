517558e989eb34c484c4cae438574237a6be637b
Mounica <71146922+SaiMounicaGudimella@users.noreply.github.com>
2025-10-02 22:21:45 -0400

    Create extract.py

---
+++ src/extract.py
@@ -0,0 +1,38 @@
+import yfinance as yf
+import duckdb
+import pandas as pd
+from datetime import datetime
+
+# 1. Download stock data
+ticker = "AAPL"
+df = yf.download(ticker, period="3y", interval="1d")
+df.reset_index(inplace=True)
+
+df.head()
+
+df.columns = df.columns.droplevel(1)
+df['Ticker'] = ticker
+
+df.head()
+
+# 2. Store in DuckDB (lightweight SQL database file)
+conn = duckdb.connect("stocks.duckdb")
+conn.execute("DROP TABLE IF EXISTS stock_data") # Drop the table if it exists
+conn.execute("CREATE TABLE stock_data AS SELECT * FROM df") # Recreate the table with the updated DataFrame
+conn.execute("INSERT INTO stock_data SELECT * FROM df")
+
+# 3. Query back
+result = conn.execute("SELECT Date, Close FROM stock_data LIMIT 5").fetchdf()
+result = conn.execute("SELECT * FROM stock_data LIMIT 5").fetchdf()
+print(result)
+
+# Add a quick signal
+df["MA50"] = df["Close"].rolling(50).mean()
+df["MA200"] = df["Close"].rolling(200).mean()
+latest = df.iloc[-1]
+if latest["MA50"] > latest["MA200"]:
+    print("📈 BUY signal for", ticker)
+else:
+    print("📉 No Buy signal yet")
+
+df.head(-5)
