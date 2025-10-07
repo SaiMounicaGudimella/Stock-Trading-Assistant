c89fc26dfc826d9b3e55bbb2f9dfb51f19f10b83
Mounica <71146922+SaiMounicaGudimella@users.noreply.github.com>
2025-10-07 15:07:09 -0400

    Update transform.py

--- src/transform.py
+++ src/transform.py
@@ -1,1 +1,9 @@
+import pandas as pd
 
+def transform_data(df):
+    df["MA50"] = df.groupby("ticker")["Close"].transform(lambda x: x.rolling(50).mean())
+    df["MA200"] = df.groupby("ticker")["Close"].transform(lambda x: x.rolling(200).mean())
+    df["Signal"] = df.apply(
+        lambda x: "BUY" if x["MA50"] > x["MA200"] else "HOLD", axis=1
+    )
+    return df
