554e668c165621cb871607e72c9546e6c95ab7f1
Mounica <71146922+SaiMounicaGudimella@users.noreply.github.com>
2025-10-07 15:07:23 -0400

    Update load.py

--- src/load.py
+++ src/load.py
@@ -1,1 +1,7 @@
+import duckdb
 
+def load_data(df, db_path="data/stocks.duckdb"):
+    conn = duckdb.connect(db_path)
+    conn.execute("CREATE TABLE IF NOT EXISTS stock_data AS SELECT * FROM df LIMIT 0")
+    conn.execute("INSERT INTO stock_data SELECT * FROM df")
+    conn.close()
